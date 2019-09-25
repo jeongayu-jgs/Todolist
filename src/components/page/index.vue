b2420671ffa5222dd66b2392efd20e8064edb1c1
ujunga <ujunga152@gmail.com>
2019-09-25 17:42:42 +0900

    Created a todolist, 19-09-25

---
+++ src/components/page/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <log-content></log-content>
+  </div>
+</template>
+<script>
+import logContent from '@/components/page/log'
+export default {
+  components: {
+    logContent
+  }
+}
+</script>
